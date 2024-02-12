55ab5818a2716f0677d425d87b5d8e0cc41950bc
Maks <mitserskyi.maksym@chnu.edu.ua>
2024-02-12 13:10:39 +0200

    3 tasks

--- ArrayFile/ArrayFile.cpp
+++ ArrayFile/ArrayFile.cpp
@@ -1,5 +1,111 @@
-﻿ // ArrayFile.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
-//
+﻿#include <iostream>
+#include <cmath>
+#include <vector>
+#include <unordered_map>
+#include <locale>
+
+using namespace std;
+
+// Task 1
+void task1() {
+  
+    int N;
+    cout << "Введіть розмір масиву N: ";
+    cin >> N;
+    int A[100];
+    cout << "Введіть елементи масиву A:" << endl;
+    for (int i = 0; i < N; ++i) {
+        cout << "A[" << i << "]: ";
+        cin >> A[i];
+    }
+    long long C[100];
+    for (int i = 0; i < N; ++i) {
+        C[i] = static_cast<long long>(pow(2, i + 1)) * A[i];
+    }
+    cout << "Масив C:" << endl;
+    for (int i = 0; i < N; ++i) {
+        cout << "C[" << i << "]: " << C[i] << endl;
+    }
+}
+
+// Task 2
+void task2() {
+    int n;
+    cout << "Введіть розмір масиву: ";
+    cin >> n;
+    vector<int> a(n);
+    for (int i = 0; i < n; i++) {
+        cout << "Введіть " << i + 1 << "-й елемент: ";
+        cin >> a[i];
+    }
+    int c, d;
+    cout << "Введіть ліву межу діапазону: ";
+    cin >> c;
+    cout << "Введіть праву межу діапазону: ";
+    cin >> d;
+    int max_idx = c;
+    for (int i = d; i >= c; i--) {
+        if (a[i] > a[max_idx]) {
+            max_idx = i;
+        }
+    }
+    if (max_idx == c) {
+        cout << "В діапазоні немає непарних чисел." << endl;
+    }
+    else {
+        cout << "Номер останнього максимального непарного елемента: " << max_idx << endl;
+    }
+}
+
+// Task 3
+void task3() {
+    cout << "Введіть розмір масиву: ";
+    int n;
+    cin >> n;
+    vector<double> a(n);
+    for (int i = 0; i < n; i++) {
+        cout << "Введіть " << i + 1 << "-й елемент: ";
+        cin >> a[i];
+    }
+    unordered_map<double, int> count;
+    for (double num : a) {
+        count[num]++;
+    }
+    double sum_repeats = 0;
+    double product_unique = 1;
+    for (auto it : count) {
+        if (it.second > 1) {
+            sum_repeats += it.first * it.second;
+        }
+        else {
+            product_unique *= it.first;
+        }
+    }
+    cout << "Сума повторюваних чисел: " << sum_repeats << endl;
+    cout << "Добуток не повторюваних чисел: " << product_unique << endl;
+}
+
+int main() {
+    setlocale(LC_CTYPE, "Ukr");
+    int task;
+    cout << "Введіть номер завдання (1, 2, або 3): ";
+    cin >> task;
+    switch (task) {
+    case 1:
+        task1();
+        break;
+    case 2:
+        task2();
+        break;
+    case 3:
+        task3();
+        break;
+    default:
+        cout << "Невірний номер завдання." << endl;
+    }
+    return 0;
+}
+
 
 #include <iostream>
 #include <fstream>
@@ -246,112 +352,3 @@ void ArrayLocal()
 //
 //
 
-#include <iostream>
-#include <cmath>
-#include <vector>
-#include <unordered_map>
-#include <locale>
-
-using namespace std;
-
-// Task 1
-void task1() {
-    setlocale(LC_CTYPE, "Ukr");
-    int N;
-    cout << "Введіть розмір масиву N: ";
-    cin >> N;
-    int A[100];
-    cout << "Введіть елементи масиву A:" << endl;
-    for (int i = 0; i < N; ++i) {
-        cout << "A[" << i << "]: ";
-        cin >> A[i];
-    }
-    long long C[100];
-    for (int i = 0; i < N; ++i) {
-        C[i] = static_cast<long long>(pow(2, i + 1)) * A[i];
-    }
-    cout << "Масив C:" << endl;
-    for (int i = 0; i < N; ++i) {
-        cout << "C[" << i << "]: " << C[i] << endl;
-    }
-}
-
-// Task 2
-void task2() {
-    int n;
-    cout << "Введіть розмір масиву: ";
-    cin >> n;
-    vector<int> a(n);
-    for (int i = 0; i < n; i++) {
-        cout << "Введіть " << i + 1 << "-й елемент: ";
-        cin >> a[i];
-    }
-    int c, d;
-    cout << "Введіть ліву межу діапазону: ";
-    cin >> c;
-    cout << "Введіть праву межу діапазону: ";
-    cin >> d;
-    int max_idx = c;
-    for (int i = d; i >= c; i--) {
-        if (a[i] > a[max_idx]) {
-            max_idx = i;
-        }
-    }
-    if (max_idx == c) {
-        cout << "В діапазоні немає непарних чисел." << endl;
-    }
-    else {
-        cout << "Номер останнього максимального непарного елемента: " << max_idx << endl;
-    }
-}
-
-// Task 3
-void task3() {
-    cout << "Введіть розмір масиву: ";
-    int n;
-    cin >> n;
-    vector<double> a(n);
-    for (int i = 0; i < n; i++) {
-        cout << "Введіть " << i + 1 << "-й елемент: ";
-        cin >> a[i];
-    }
-    unordered_map<double, int> count;
-    for (double num : a) {
-        count[num]++;
-    }
-    double sum_repeats = 0;
-    double product_unique = 1;
-    for (auto it : count) {
-        if (it.second > 1) {
-            sum_repeats += it.first * it.second;
-        }
-        else {
-            product_unique *= it.first;
-        }
-    }
-    cout << "Сума повторюваних чисел: " << sum_repeats << endl;
-    cout << "Добуток не повторюваних чисел: " << product_unique << endl;
-}
-
-int main() {
-    // Choose which task to execute
-    int task;
-    cout << "Введіть номер завдання (1, 2, або 3): ";
-    cin >> task;
-
-    switch (task) {
-    case 1:
-        task1();
-        break;
-    case 2:
-        task2();
-        break;
-    case 3:
-        task3();
-        break;
-    default:
-        cout << "Невірний номер завдання." << endl;
-    }
-
-    return 0;
-}
